54a14d772ee6c14cbcf4f7202720ccd5259eabab
Harshify <95436289+Har-12@users.noreply.github.com>
2024-05-08 12:35:06 +0530

    Create 1911. Maximum Alternating Subsequence Sum.java

---
+++ 1911. Maximum Alternating Subsequence Sum.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public long maxAlternatingSum(int[] nums) {
+        long f = 0, g = 0;
+        for (int x : nums) {
+            long ff = Math.max(g - x, f);
+            long gg = Math.max(f + x, g);
+            f = ff;
+            g = gg;
+        }
+        return Math.max(f, g);
+    }
+}
