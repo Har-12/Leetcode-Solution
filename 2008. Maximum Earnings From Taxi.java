040b3ce43ce1dcb4321d3d7a68f96558fdffd3c5
Harshify <95436289+Har-12@users.noreply.github.com>
2024-05-14 12:06:28 +0530

    Create 2008. Maximum Earnings From Taxi.java

--- 2008. Maximum Earnings From Taxi.java
+++ 2008. Maximum Earnings From Taxi.java
@@ -1,21 +1,29 @@
-// OJ: https://leetcode.com/problems/maximum-earnings-from-taxi/
-// Time: O(N + M) where `N` is number of points and `M` is the length of `A`.
-// Space: O(N)
 class Solution {
-public:
-    long long maxTaxiEarnings(int n, vector<vector<int>>& A) {
-        vector<vector<pair<int, int>>> rideStartAt(n); // group all the rides starting at the same time
-        for (auto &ride : A) {
-            int s = ride[0], e = ride[1], t = ride[2];
-            rideStartAt[s].push_back({ e, e - s + t });  // [end, dollar]
-        }
-        vector<long long> dp(n + 1);
-        for (int i = n - 1; i >= 1; --i) { // Traverse the rides in descending order of start time
-            for (auto &[e, d] : rideStartAt[i]) {
-                dp[i] = max(dp[i], dp[e] + d);
-            }
-            dp[i] = max(dp[i], dp[i + 1]);
-        }
-        return dp[1];
+  public long maxTaxiEarnings(int n, int[][] rides) {
+    List<Pair<Integer, Integer>>[] startToEndAndEarns = new List[n];
+    // dp[i] := the maximum dollars you can earn starting at i
+    long[] dp = new long[n + 1];
+
+    for (int i = 1; i < n; ++i)
+      startToEndAndEarns[i] = new ArrayList<>();
+
+    for (int[] ride : rides) {
+      final int start = ride[0];
+      final int end = ride[1];
+      final int tip = ride[2];
+      final int earn = end - start + tip;
+      startToEndAndEarns[start].add(new Pair<>(end, earn));
     }
-};
+
+    for (int i = n - 1; i >= 1; --i) {
+      dp[i] = dp[i + 1];
+      for (Pair<Integer, Integer> pair : startToEndAndEarns[i]) {
+        final int end = pair.getKey();
+        final int earn = pair.getValue();
+        dp[i] = Math.max(dp[i], dp[end] + earn);
+      }
+    }
+
+    return dp[1];
+  }
+}
